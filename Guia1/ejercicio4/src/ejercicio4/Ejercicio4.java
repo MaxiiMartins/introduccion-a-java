42569e38773a8c6b1b3afeaa220cb6f4ae746a9b
Maximiliano <maximartins45@gmail.com>
2023-06-19 16:06:02 -0300

    guia 1 ejercicio 4

---
+++ Guia1/ejercicio4/src/ejercicio4/Ejercicio4.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio4;
+
+/**
+ *
+ * @author defaultuser0
+ */
+public class Ejercicio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Perro perro = new Perro("Olivia", "Buldog Francés", 1.2, 1);
+
+        perro.mostrarInfo();
+    }
+    
+}
